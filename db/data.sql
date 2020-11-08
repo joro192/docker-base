f6cd5a9e1d478e646b3c56f418ff2d211c6d59d0
Joro <joro010920@gmail.com>
2020-11-09 01:22:14 +0700

    first commit

---
+++ db/data.sql
@@ -0,0 +1,70 @@
+  
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Jun 27, 2019 at 01:14 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+07:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `docker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `docker`
+--
+
+CREATE TABLE `docker` (
+  `id` int(255) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `docker`
+--
+
+INSERT INTO `docker` (`id`, `name`) VALUES
+(1, 'andrew'),
+(2, 'brian'),
+(3, 'charles'),
+(4, 'david');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `docker`
+--
+ALTER TABLE `docker`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `docker`
+--
+ALTER TABLE `docker`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
